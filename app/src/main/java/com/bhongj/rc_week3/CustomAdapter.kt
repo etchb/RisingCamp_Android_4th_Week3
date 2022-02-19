8a6141d189ff77369656345766eed0424c3c13f6
etchb <bhongj91@gmail.com>
2022-02-19 16:25:54 +0900

    Initial commit + ListView example

---
+++ app/src/main/java/com/bhongj/rc_week3/CustomAdapter.kt
@@ -0,0 +1,30 @@
+package com.bhongj.rc_week3
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.bhongj.rc_week3.databinding.ListviewItemBinding
+
+class CustomAdapter(context: Context, private val businessCardArrayList: ArrayList<BusinessCard>):BaseAdapter() {
+
+    private lateinit var binding: ListviewItemBinding
+    private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount() : Int = businessCardArrayList.size
+
+    override fun getItem(p0: Int) : Any = businessCardArrayList[p0]
+
+    override fun getItemId(p0: Int): Long = p0.toLong()
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+
+        binding = ListviewItemBinding.inflate(inflater, p2, false)
+
+        binding.listviewTxtName.text = businessCardArrayList[p0].name
+        binding.listviewTxtContent.text = businessCardArrayList[p0].contents
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/bhongj/rc_week3/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.bhongj.rc_week3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bhongj.rc_week3.databinding.ActivityMainBinding
+
+data class BusinessCard(val name:String, val contents:String)
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    var businessCardArrayList = ArrayList<BusinessCard>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        for(i in 1..10) {
+            businessCardArrayList.add(BusinessCard(name = "kim", contents = "010-1234-1234"))
+            businessCardArrayList.add(BusinessCard(name = "park", contents = "010-4567-4567"))
+        }
+
+        val customAdapter = CustomAdapter(this, businessCardArrayList)
+        binding.listviewMain.adapter = customAdapter
+    }
+}
